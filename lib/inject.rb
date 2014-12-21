cb268704de26fc2c43d20ab52d4c794a9dc2c16f
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-21 12:54:08 +0000

    Testing addition with a symbol and an argument

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,14 +1,27 @@
 class Array
 
 	def my_inject(*parameter)
-		memo = self.shift 
-		(self.count).times do
-			n = self.shift 
-			if parameter[0].is_a? Symbol
-			   memo = memo.send(parameter[0], n) 
-			else
-			   memo = yield(memo, n) 
-			end
+		
+		if parameter[0].is_a? Integer
+			   memo = parameter[0]
+			   (self.count).times do
+			    	n = self.shift 
+					if parameter[1].is_a? Symbol
+			   			memo = memo.send(parameter[1], n) 
+					else
+			   			memo = yield(memo, n) 
+					end
+				end
+		else
+				memo = self.shift  
+				(self.count).times do
+					n = self.shift 
+					if parameter[0].is_a? Symbol
+			   			memo = memo.send(parameter[0], n) 
+					else
+			 		    memo = yield(memo, n) 
+					end
+				end
 		end
 		memo
 	end
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -5,18 +5,18 @@ describe Array do
 	context 'with a block' do
 	
 		it 'should sum numbers' do
-			expect([1,2,3,4].inject{ |memo, n| memo + n }).to eq(10)
-			expect([1,2,3,4].my_inject{ |memo, n| memo + n }).to eq(10)
+			expect([1,2,3,4].inject { |memo, n| memo + n }).to eq(10)
+			expect([1,2,3,4].my_inject { |memo, n| memo + n }).to eq(10)
 		end
 
 		it 'should subtract numbers' do
-			expect([1,2,3,4].inject{ |memo, n| memo - n }).to eq(-8)
-			expect([1,2,3,4].my_inject{ |memo, n| memo - n }).to eq(-8)
+			expect([1,2,3,4].inject { |memo, n| memo - n }).to eq(-8)
+			expect([1,2,3,4].my_inject { |memo, n| memo - n }).to eq(-8)
 		end
 
 		it 'should multiply numbers' do
-			expect([1,2,3,4].inject{ |memo, n| memo * n }).to eq(24)
-			expect([1,2,3,4].my_inject{ |memo, n| memo * n }).to eq(24)
+			expect([1,2,3,4].inject { |memo, n| memo * n }).to eq(24)
+			expect([1,2,3,4].my_inject { |memo, n| memo * n }).to eq(24)
 		end
 
 	end
@@ -40,4 +40,25 @@ describe Array do
 	
 	end
 
+	context 'with a symbol and an argument' do
+
+		it 'should sum numbers' do
+			expect([1,2,3,4].inject(2, :+)).to eq(12)
+			expect([1,2,3,4].my_inject(2, :+)).to eq(12)
+		end
+
+	end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
