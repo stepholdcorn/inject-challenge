54f79dd7e8e2a5b6fa71c40f197331b058c24a0e
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-21 11:57:51 +0000

    Testing subtraction and multiplication with a symbol

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -27,6 +27,16 @@ describe Array do
 			expect([1,2,3,4].inject(:+)).to eq(10)
 			expect([1,2,3,4].my_inject(:+)).to eq(10)
 		end
+
+		it 'should subtract numbers' do
+			expect([1,2,3,4].inject(:-)).to eq(-8)
+			expect([1,2,3,4].my_inject(:-)).to eq(-8)
+		end
+
+		it 'should subtract numbers' do
+			expect([1,2,3,4].inject(:*)).to eq(24)
+			expect([1,2,3,4].my_inject(:*)).to eq(24)
+		end
 	
 	end
 
