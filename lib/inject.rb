5731078187349c5362c9342558f76fc9334b134a
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-03 17:08:19 +0000

    Refactoring

--- lib/inject.rb
+++ lib/inject.rb
@@ -2,27 +2,18 @@ class Array
 
 	def my_inject(*parameter)
 		copy = self.dup
-		
 		if parameter[0].is_a? Integer
-			   memo = parameter[0]
-			   (copy.count).times do
-			    	n = copy.shift 
-					if parameter[1].is_a? Symbol
-			   			memo = memo.send(parameter[1], n) 
-					else
-			   			memo = yield(memo, n) 
-					end
-				end
+		   memo = parameter[0]
+		   (copy.count).times do
+		    	n = copy.shift 
+				(parameter[1].is_a? Symbol) ? memo = memo.send(parameter[1], n) : memo = yield(memo, n) 
+			end
 		else
-				memo = copy.shift  
-				(copy.count).times do
-					n = copy.shift 
-					if parameter[0].is_a? Symbol
-			   			memo = memo.send(parameter[0], n) 
-					else
-			 		    memo = yield(memo, n) 
-					end
-				end
+			memo = copy.shift  
+			(copy.count).times do
+				n = copy.shift 
+				(parameter[0].is_a? Symbol) ? memo = memo.send(parameter[0], n) : memo = yield(memo, n)
+			end
 		end
 		memo
 	end
