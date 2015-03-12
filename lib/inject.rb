17047b23e5ed1eef93dc0f2e560e232f8e77f175
steph <stephieoldcorn@gmail.com>
2015-03-12 16:01:44 +0000

    Spring cleaning

--- lib/inject.rb
+++ lib/inject.rb
@@ -3,15 +3,15 @@ class Array
 	def my_inject(*parameter)
 		copy = self.dup
 		if parameter[0].is_a? Integer
-		   memo = parameter[0]
-		   index = 1		   
+		  memo = parameter[0]
+		  index = 1		   
 		else
 			memo = copy.shift 
 			index = 0
 		end 
 		(copy.count).times do
-		    n = copy.shift 
-			(parameter[index].is_a? Symbol) ? memo = memo.send(parameter[index], n) : memo = yield(memo, n) 
+      n = copy.shift 
+      (parameter[index].is_a? Symbol) ? memo = memo.send(parameter[index], n) : memo = yield(memo, n) 
 		end
 		memo
 	end
